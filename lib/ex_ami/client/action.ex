de269c4801c6c09aa38d887ead0fc788b355751b
Stephen Pallen <steve.pallen@emetrotel.org>
2015-09-16 09:26:44 -0400

    added new action module with hangup action

---
+++ lib/ex_ami/client/action.ex
@@ -0,0 +1,9 @@
+defmodule ExAmi.Client.Action do
+
+  def hangup(client, channel, callback \\ nil) do
+    action = ExAmi.Message.new_action "Hangup", [{"Channel", channel}]
+    pid = Process.whereis client
+    ExAmi.Client.send_action pid, action, callback
+  end
+  
+end
